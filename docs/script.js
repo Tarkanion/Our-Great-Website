36e583d495bc2650530ec3e33fa85c79748d3248
ChungusBeliever <ChungusBeliever@users.noreply.github.com>
2025-06-12 03:03:09 -0700

    Started verification checkbox

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,26 @@
-const sound = new Audio("resources/beep.mp3")
+const sound = new Audio("resources/beep.mp3");
+const soundButton = document.getElementById("soundButton");
+const verificationReminder = document.getElementById("verification-reminder");
 
+// EFFECTS: plays sound if sound button is verified
 function play() {
-    sound.play();
+    if (isVerified()) {
+        sound.play();
+    }
 }
 
+// EFFECTS: returns true if sound button is verified, otherwise 
+// creates please verify popup if non-existent
+function isVerified() {
+    if (soundButton.classList.contains("verified")) {
+        return true;
+    } else if (!verificationReminder.classList.contains("active")) {
+        const verifyNow = document.createElement("p");
+        verifyNow.textContent = "Verify NOW";
+        verifyNow.style.color = "red";
+        verificationReminder.appendChild(verifyNow);
+        verificationReminder.classList.add("active");
+    }
+}
 
-document.getElementById("beep").addEventListener("click", play)
+soundButton.addEventListener("click", play);
