ae7242cde984add3e8023b1272b8aac8577d208f
ChungusBeliever <ChungusBeliever@users.noreply.github.com>
2025-06-13 03:05:52 -0700

    Fixed base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "Our-Great-Website"
+  base: "/Our-Great-Website/"
 })
