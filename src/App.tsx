ef3580f57baa38b6c78a6a71e99222ee55fff041
ChungusBeliever <ChungusBeliever@users.noreply.github.com>
2025-06-13 02:31:49 -0700

    Converted old ui to use react

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import beep from "./assets/beep.mp3"
 
 function App() {
-  const [count, setCount] = useState(0)
+    const playSound = () => {
+        const sound = new Audio(beep);
+        sound.play();
+    }
 
-  return (
+    return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div id="content">
+            <h2>Hello World</h2>
+            <button id="soundButton" onClick={playSound}>Click Me</button>
+            <div id="verification-reminder"></div>
+        </div>
     </>
-  )
+    )
 }
+
 
 export default App
